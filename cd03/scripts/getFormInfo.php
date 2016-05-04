d872ab7cac16eb5dcb2d9407004b93b6047bf0a8
Gokhan Derala <gderala@gmail.com>
2016-05-04 09:03:09 +0300

    php test

--- cd03/scripts/getFormInfo.php
+++ cd03/scripts/getFormInfo.php
@@ -1,3 +1,27 @@
+<?php 
+	$firstname = $_REQUEST["first_name"];
+	$lastname = $_REQUEST["last_name"];
+	$email = $_REQUEST["email"];
+	$facebook = $_REQUEST["facebook_url"];
+	$position = strpos($facebook, "facebook.com");
+	/*if($position==false) {
+		$facebook = "https://www.facebook.com/". $facebook;
+	}*/
+
+	if($position===false){
+		$facebook = "http://www.facebook.com/".$facebook;
+	}
+	//echo var_dump($position); exit();
+	$twitter = $_REQUEST["twitter_handle"];
+	$twitter_url = "http://www.twitter.com/";
+	$position = strpos($twitter, '@');
+	if($position===false){
+		$twitter = $twitter_url . $twitter;
+	}else {
+		//Do something to remove the @ from the twitter handle
+		$twitter_url = $twitter_url . substr($twitter, $position + 1);
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,11 +32,10 @@
 	<div id="example">Example 3-1</div>
 	<div id="content">
 		<p>Here is a record of what information you submitted:</p>
-		<p>First Name: <?php $_REQUEST['first_name']; ?><br />
-			Last Name: <?php echo $_REQUEST['last_name']; ?><br />
-			Email: <?php echo $_REQUEST['email']; ?><br />
-			Facebook URL: <?php echo $_REQUEST['facebook_url']; ?><br />
-			Twitter: <?php echo $_REQUEST['twitter_handle']; ?><br />
+		<p>Full Name: <?php echo $firstname." ".$lastname; ?><br />
+			Email: <?php echo $email ; ?><br />
+			<a href="<?php echo $facebook ; ?>">Check your Facebook Page</a><br />
+			<a href="<?php echo $twitter_url ;?>">Check your Twitter Account</a><br />
 			</p>
 	</div>
 
