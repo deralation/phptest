47f941c75091f8ea8a76b9677fcfc0a221d054ce
Gokhan Derala <gderala@gmail.com>
2016-09-23 00:36:30 +0300

    test

---
+++ phpManual/helloWorld.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+</head>
+<body>
+
+<script type="text/javascript">
+	document.write("Hello World")
+</script>
+
+<noscript>
+	Your browser doesn't support or has disabled Javascript	
+</noscript>
+
+</body>
+</html>
